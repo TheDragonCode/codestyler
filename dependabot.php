0c270b743b3c4038ffd6bd39c2c29b5c6b7c70ca
Andrey Helldar <helldar@ai-rus.com>
2022-02-09 23:56:27 +0300

    Improved work of dependabot maker

--- dependabot.php
+++ dependabot.php
@@ -2,31 +2,87 @@
 
 declare(strict_types=1);
 
-$target_path = './.github/dependabot.yml';
+class Dependabot
+{
+    protected array $update = [
+        'package-ecosystem' => 'github-actions',
 
-$content = file_exists($target_path) ? yaml_parse_file($target_path) : [];
+        'directory' => '/',
 
-$updates = $content['updates'] ?? [];
+        'schedule' => [
+            'interval' => 'daily',
+            'time'     => '00:00',
+        ],
+    ];
 
-foreach ($updates as $update) {
-    if ($update['package-ecosystem'] === 'github-actions') {
-        exit(0);
+    protected array $content = [];
+
+    protected int $version = 2;
+
+    protected string $name = 'github-actions';
+
+    public function __construct(
+        protected string $path
+    ) {
+        $this->content = $this->parse();
+    }
+
+    public function handle(): void
+    {
+        $updates = $this->each($this->getUpdates());
+        $version = $this->getVersion();
+
+        $this->store($version, $updates);
+    }
+
+    protected function each(array $updates): array
+    {
+        $found = false;
+
+        foreach ($updates as &$update) {
+            if ($update['package-ecosystem'] === $this->name) {
+                $update = $this->update;
+                $found  = true;
+                break;
+            }
+        }
+
+        return $found ? $updates : $this->put($updates);
+    }
+
+    protected function put(array $updates): array
+    {
+        $updates[] = $this->update;
+
+        return $updates;
+    }
+
+    protected function store(int $version, array $updates): void
+    {
+        yaml_emit_file($this->path, compact('version', 'updates'));
+    }
+
+    protected function getUpdates(): array
+    {
+        return $this->content['updates'] ?? [];
+    }
+
+    protected function getVersion(): int
+    {
+        return $this->version;
+    }
+
+    protected function parse(): array
+    {
+        return $this->exists() ? yaml_parse_file($this->path) : [];
+    }
+
+    protected function exists(): bool
+    {
+        return file_exists($this->path);
     }
 }
 
-$updates[] = [
-    'package-ecosystem' => 'github-actions',
+$dependabot = new Dependabot('./.github/dependabot.yml');
 
-    'directory' => '/',
-
-    'schedule' => [
-        'interval' => 'daily',
-        'time'     => '00:00',
-        'timezone' => 'Etc/GMT'
-    ],
-];
-
-$content['version'] = 2;
-$content['updates'] = $updates;
-
-yaml_emit_file($target_path, $content);
+$dependabot->handle();
