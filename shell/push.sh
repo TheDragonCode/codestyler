2f65085264a991087909ce30e3b0f85fdfa79b55
Andrey Helldar <helldar@ai-rus.com>
2022-02-10 22:10:20 +0300

    Fixed condition

--- shell/push.sh
+++ shell/push.sh
@@ -1,5 +1,17 @@
 #!/bin/sh -l
 
-if [[ [$(allowFix) == "true"] && ["$IS_DIRTY_DEPENDABOT" == "1" || "$IS_DIRTY_EDITORCONFIG" == "1" || "$IS_DIRTY_CODE" == "1"] ]]; then
-     git push
+function isDirty() {
+    local result="false"
+
+    if [[ $(allowFix) == "true" ]]; then
+        if [[ "$IS_DIRTY_DEPENDABOT" == "1" || "$IS_DIRTY_EDITORCONFIG" == "1" || "$IS_DIRTY_CODE" == "1" ]]; then
+            result="true"
+        fi
+    fi
+
+    echo "$result"
+}
+
+if [[ $(allowFix) == "true" && $(isDirty) == "true" ]]; then
+    git push
 fi
