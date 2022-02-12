4c00dae3d910ed796ef07d044a3d380f08a7382a
Andrey Helldar <helldar@ai-rus.com>
2022-02-12 17:46:13 +0300

    Bin file renamed to `codestyle`

--- shell/dependabot.sh
+++ shell/dependabot.sh
@@ -5,5 +5,5 @@ IS_DIRTY_DEPENDABOT=0
 if [[ $(allowFix) == "true" && $(allow "$INPUT_DEPENDABOT") == "true" ]]; then
     IS_DIRTY_DEPENDABOT=1
 
-    { codestyler dependabot && git add . && git commit -a -m "Update Dependabot 🤖"; } || IS_DIRTY_DEPENDABOT=0
+    { codestyle dependabot && git add . && git commit -a -m "Update Dependabot 🤖"; } || IS_DIRTY_DEPENDABOT=0
 fi
