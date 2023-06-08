0a12aa8bd9e9b697b207b51c2ded8a00dd0fd938
Andrey Helldar <helldar@dragon-code.pro>
2023-06-08 19:30:45 +0300

    Update presets

--- resources/presets/8.2.php
+++ resources/presets/8.2.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use DragonCode\CodeStyler\Factories\ConfigurationFactory;
 
-$default = require __DIR__ . '/default.php';
+$default = require base_path('resources/presets/default.php');
 
 return ConfigurationFactory::preset(array_merge($default, [
     '@PHP82Migration' => true,
