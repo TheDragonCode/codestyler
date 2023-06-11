f464eff9a853fb90243195a3c2a2df439f712361
Andrey Helldar <helldar@dragon-code.pro>
2023-06-11 15:51:08 +0300

    Fixed persmissions for run plugins

--- shell/normalize.sh
+++ shell/normalize.sh
@@ -5,6 +5,8 @@ IS_DIRTY_NORMALIZE=0
 if [[ $(allow "$INPUT_NORMALIZE") == "true" ]]; then
     composer global require ergebnis/composer-normalize
 
+    composer config --no-plugins allow-plugins.ergebnis/composer-normalize true
+
     if [[ -f "./composer.json" ]]; then
         IS_DIRTY_NORMALIZE=1
 
--- shell/plugins.sh
+++ shell/plugins.sh
@@ -4,8 +4,8 @@ IS_PLUGINS_CONFIG=1
 
 composer global config --no-plugins allow-plugins.dragon-code/codestyler true
 composer global config --no-plugins allow-plugins.ergebnis/composer-normalize true
+composer global config --no-plugins allow-plugins.symfony/thanks true
 
 composer config --no-plugins allow-plugins.dragon-code/codestyler true
-composer config --no-plugins allow-plugins.ergebnis/composer-normalize true
 
-{ git add composer.json && git commit -a -m "💂 Updated permissions to run plugins"; } || IS_PLUGINS_CONFIG=0
+{ git add composer.json composer.lock && git commit -a -m "💂 Updated permissions to run code styler"; } || IS_PLUGINS_CONFIG=0
