7a5535858450088387d772bdc889f18df4045084
Andrey Helldar <helldar@dragon-code.pro>
2025-08-27 17:20:46 +0300

    Added examples file

---
+++ tests/style.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+class EmptyClass {}
+
+class ConstructorClass
+{
+    public const First  = 0;
+    public const Second = 1;
+
+    public int $firstValue = 1;
+
+    public int $secondValue = 1;
+
+    public function __construct(
+        protected int|string $value
+    ) {}
+}
+
+class ValueClass
+{
+    /**
+     * @param  int|string  $item  Some Value
+     */
+    public function value(int|string $item): int|string
+    {
+        $result = static fn () => $item;
+
+        // Return value
+        return $result();
+    }
+
+    public function alignment(): array
+    {
+        return [
+            'foo'    => 'Foo',
+            'bar'    => 'Bar',
+            'qwerty' => 'Qwerty',
+
+            'f1'   => 'F1',
+            'f2'   => 'F2',
+            'some' => 'Some',
+        ];
+    }
+}
+
+$newClass = new ValueClass;
